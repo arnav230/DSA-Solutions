8838ef4ae650ddad4696872fc65a9496cc043c7f
Arnav Gupta <arnavg2302@gmail.com>
2026-05-20 14:31:06 +0530

    Create lc207_courseschedule.cpp

---
+++ Graphs/lc207_courseschedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> indegree(numCourses, 0);
+        for(auto &it : prerequisites) {
+            int a = it[0];
+            int b = it[1];
+
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        int count = 0;
+        // kahn's algorithm
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+            for(auto &it : adj[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        // if all courses completed
+        return count == numCourses;
+    }
+};
