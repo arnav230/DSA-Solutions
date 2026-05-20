0b167be6374c85429584a4a86f05dcb1b939f267
Arnav Gupta <arnavg2302@gmail.com>
2026-05-20 15:23:07 +0530

    Create lc118_pasacl's triangle.cpp

---
+++ Arrays/lc118_pasacl's triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> fans;
+        for (int i = 1; i <= numRows; i++) {
+            vector<int> temp;
+            int ans = 1;
+            temp.push_back(ans);
+            for (int j = 1; j < i; j++) {
+                ans *= i - j;
+                ans = ans / j;
+                temp.push_back(ans);
+            }
+            fans.push_back(temp);
+        }
+        return fans;
+    }
+};
