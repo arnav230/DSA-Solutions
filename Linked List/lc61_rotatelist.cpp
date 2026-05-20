39e663ab4b03482efd3160acff0b2621140c17b0
Arnav Gupta <arnavg2302@gmail.com>
2026-05-20 15:13:57 +0530

    Create lc61_rotatelist.cpp

---
+++ Linked List/lc61_rotatelist.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) 
+            return head;
+        int len = 1;
+        ListNode* tail = head;
+        while (tail->next) {
+            tail = tail->next;
+            len++;
+        }
+        k = k % len;
+        if (k == 0) 
+            return head;
+        tail->next = head;
+
+        int stepsToNewTail = len - k - 1;
+        ListNode* newTail = head;
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail->next;
+        }
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+        return newHead;
+    }
+};
